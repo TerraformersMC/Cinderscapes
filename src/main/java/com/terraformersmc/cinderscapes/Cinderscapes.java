b4942cce7b2f3652dc4418ff3b71353e5e324b61
SuperCoder7979 <k.pranav@gmail.com>
2020-04-16 13:39:15 -0400

    intialize project

---
+++ src/main/java/com/terraformersmc/cinderscapes/Cinderscapes.java
@@ -0,0 +1,10 @@
+package com.terraformersmc.cinderscapes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cinderscapes implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/terraformersmc/cinderscapes/CinderscapesClient.java
@@ -0,0 +1,13 @@
+package com.terraformersmc.cinderscapes;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.Environment;
+import net.fabricmc.api.EnvType;
+
+@Environment(EnvType.CLIENT)
+public class CinderscapesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
