14badc4354c3fbd480cd3c379545d4ca9b2f4d15
Brandon Davis <neusfear@gmail.com>
2020-04-18 19:58:49 -0500

    Luminous forest textures

--- src/main/java/com/terraformersmc/cinderscapes/client/CinderscapesClient.java
+++ src/main/java/com/terraformersmc/cinderscapes/client/CinderscapesClient.java
@@ -1,5 +1,6 @@
 package com.terraformersmc.cinderscapes.client;
 
+import com.terraformersmc.cinderscapes.Cinderscapes;
 import com.terraformersmc.cinderscapes.init.CinderscapesBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
@@ -11,6 +12,7 @@ import net.minecraft.client.render.RenderLayer;
 public class CinderscapesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getTranslucent(), CinderscapesBlocks.UMBRAL_WART_BLOCK);
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), CinderscapesBlocks.FLAY, CinderscapesBlocks.WITHERED_FUNGUS, CinderscapesBlocks.RADIANT_ROOTS, CinderscapesBlocks.PHOTOFERN, CinderscapesBlocks.TALL_PHOTOFERN, CinderscapesBlocks.LUMINOUS_POD, CinderscapesBlocks.TWILIGHT_FESCUES, CinderscapesBlocks.TWILIGHT_TENDRILS, CinderscapesBlocks.UMBRAL_FUNGUS);
     }
 }
