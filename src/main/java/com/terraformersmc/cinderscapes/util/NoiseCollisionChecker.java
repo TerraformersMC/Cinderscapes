b8699c502afed505c8f67ad3abc3185d4a330cd5
Prospector <prospectordev@gmail.com>
2020-06-25 00:25:28 -0700

    Remove unnecessary reflection #change "Remove some unnecessary reflection throwing errors" #release beta

--- src/main/java/com/terraformersmc/cinderscapes/util/NoiseCollisionChecker.java
+++ src/main/java/com/terraformersmc/cinderscapes/util/NoiseCollisionChecker.java
@@ -1,11 +1,10 @@
 package com.terraformersmc.cinderscapes.util;
 
+import com.terraformersmc.cinderscapes.mixin.BiomeAccessor;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
-import net.minecraft.text.TranslatableText;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +12,7 @@ import java.util.Map;
 //TODO: Check
 @SuppressWarnings("unchecked")
 public class NoiseCollisionChecker {
-    private static Map<Biome, Long> Hashes = new HashMap<>();
+    private static final Map<Biome, Long> HASHES = new HashMap<>();
 
     public static void init() {
         for (Biome biome : Registry.BIOME) {
@@ -30,28 +29,22 @@ public class NoiseCollisionChecker {
     }
 
     private static void check(Biome biome) {
-        //use cursed reflection to make sure no biomes have noise collisions
-        try {
-            Field noisePoints = Biome.class.getDeclaredField("noisePoints");
-            noisePoints.setAccessible(true);
-            List<Biome.MixedNoisePoint> points = (List<Biome.MixedNoisePoint>) noisePoints.get(biome);
-            //biomes can have multiple noise points so we hash all of them
-            long hash = 0;
-            for (Biome.MixedNoisePoint point : points) {
-                hash += point.hashCode();
-            }
-
-            // ensure the biomes have distinct hashes
-            long finalHash = hash;
-            Hashes.forEach((cachedBiome, biomeHash) -> {
-                if (finalHash == biomeHash) {
-                    System.out.println("WARNING: " + biome.getTranslationKey() + " and " + cachedBiome.getTranslationKey() + " have the same mixed noise points! They won't generate properly!!!!");
-                }
-            });
-
-            Hashes.put(biome, hash);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
+        List<Biome.MixedNoisePoint> points = ((BiomeAccessor) biome).getNoisePoints();
+        //biomes can have multiple noise points so we hash all of them
+        long hash = 0;
+        for (Biome.MixedNoisePoint point : points) {
+            hash += point.hashCode();
         }
+
+        // ensure the biomes have distinct hashes
+        long finalHash = hash;
+        HASHES.forEach((cachedBiome, biomeHash) -> {
+            if (finalHash == biomeHash) {
+                System.out.println("WARNING: " + biome.getTranslationKey() + " and " + cachedBiome.getTranslationKey() + " have the same mixed noise points! They won't generate properly!!!!");
+            }
+        });
+
+        HASHES.put(biome, hash);
+
     }
 }
