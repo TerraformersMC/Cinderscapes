ea73f0f990be2da5d166f418b4a8af95c8668d04
Hugman <contact.hugman@gmail.com>
2020-07-06 00:07:08 +0200

    Updated pyracinth shape

--- src/main/java/com/terraformersmc/cinderscapes/block/PyracinthBlock.java
+++ src/main/java/com/terraformersmc/cinderscapes/block/PyracinthBlock.java
@@ -4,11 +4,12 @@ import com.terraformersmc.cinderscapes.util.StateShapeSupplier;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -19,7 +20,7 @@ import java.util.Random;
  */
 public class PyracinthBlock extends CinderscapesNetherPlantBlock {
     public PyracinthBlock() {
-        super(FabricBlockSettings.copy(Blocks.WARPED_ROOTS), (state) -> Block.createCuboidShape(2.0, 0.0, 2.0, 14.0, 12.0, 14.0));
+        super(FabricBlockSettings.copy(Blocks.WARPED_ROOTS), (state) -> Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 16.0, 11.0));
     }
 
     @Environment(EnvType.CLIENT)
@@ -28,5 +29,12 @@ public class PyracinthBlock extends CinderscapesNetherPlantBlock {
         if (random.nextFloat() > 0.6) {
             world.addParticle(ParticleTypes.FLAME, pos.getX() + 0.4f + random.nextFloat() * 0.2f, pos.getY() + 0.5f, pos.getZ() + 0.4f + random.nextFloat() * 0.2f, 0.0f, 0.05f, 0.0f);
         }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        Vec3d vec3d = state.getModelOffset(world, pos);
+        return SHAPE_SUPPLIER.apply(state).offset(vec3d.x, vec3d.y, vec3d.z);
     }
 }
