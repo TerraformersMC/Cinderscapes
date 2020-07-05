edadc1094e0db85488ece8fbcb5aad387a340cb6
Hugman <contact.hugman@gmail.com>
2020-07-06 01:10:52 +0200

    Added crystinium offset

--- src/main/java/com/terraformersmc/cinderscapes/block/CrystiniumBlock.java
+++ src/main/java/com/terraformersmc/cinderscapes/block/CrystiniumBlock.java
@@ -7,8 +7,12 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 
 import java.util.Random;
@@ -27,5 +31,12 @@ public class CrystiniumBlock extends CinderscapesNetherPlantBlock {
         if (random.nextFloat() > 0.8) {
             world.addParticle(ParticleTypes.FIREWORK, pos.getX() + random.nextFloat(), pos.getY() + random.nextFloat(), pos.getZ() + random.nextFloat(), random.nextFloat() * 0.1 - 0.05, random.nextFloat() * 0.1 - 0.05, random.nextFloat() * 0.1 - 0.05);
         }
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        Vec3d modelOffset = state.getModelOffset(world, pos);
+        return SHAPE_SUPPLIER.apply(state).offset(modelOffset.x, modelOffset.y, modelOffset.z);
     }
 }
