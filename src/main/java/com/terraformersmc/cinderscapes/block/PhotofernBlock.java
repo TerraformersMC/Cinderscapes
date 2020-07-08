1c39f9afb5c8229028c4dfaf25ce267a47ff1f19
Will Toll <will@jtoll.com>
2020-07-08 14:41:28 -0500

    Update PhotofernBlock.java

--- src/main/java/com/terraformersmc/cinderscapes/block/PhotofernBlock.java
+++ src/main/java/com/terraformersmc/cinderscapes/block/PhotofernBlock.java
@@ -19,17 +19,20 @@ public class PhotofernBlock extends CinderscapesNetherPlantBlock implements Fert
 		super(FabricBlockSettings.copy(Blocks.WARPED_ROOTS), (state) -> Block.createCuboidShape(2.0, 0.0, 2.0, 14.0, 12.0, 14.0));
 	}
 
+	@Override
 	public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
 		return true;
 	}
 
+	@Override
 	public boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
 		return true;
 	}
 
+	@Override
 	public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
 		CinderscapesNetherTallPlantBlock tallPhotofern = (CinderscapesNetherTallPlantBlock) CinderscapesBlocks.TALL_PHOTOFERN;
-		if(tallPhotofern.getDefaultState().canPlaceAt(world, pos) && world.isAir(pos.up())) {
+		if (tallPhotofern.getDefaultState().canPlaceAt(world, pos) && world.isAir(pos.up())) {
 			tallPhotofern.placeAt(world, pos, 2);
 		}
 	}
