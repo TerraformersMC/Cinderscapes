a57b568f840230c78e1458b7ac52070910d3c6d1
Vaerian <vaeriann@gmail.com>
2020-10-13 01:07:56 -0600

    Added a check for the biome in the ash placer
    Closes #50

--- src/main/java/com/terraformersmc/cinderscapes/feature/AshTopLayerFeature.java
+++ src/main/java/com/terraformersmc/cinderscapes/feature/AshTopLayerFeature.java
@@ -1,15 +1,22 @@
 package com.terraformersmc.cinderscapes.feature;
 
+import com.terraformersmc.cinderscapes.biome.AshyShoalsBiome;
+import com.terraformersmc.cinderscapes.init.CinderscapesBiomes;
 import com.terraformersmc.cinderscapes.init.CinderscapesBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.registry.BuiltinRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.StructureWorldAccess;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 
+import java.util.Optional;
 import java.util.Random;
 
 /**
@@ -28,7 +35,10 @@ public class AshTopLayerFeature extends Feature<DefaultFeatureConfig> {
                 for (int y = 0; y < 128; y++) {
                     BlockPos testPosition = pos.add(x, y, z);
                     BlockState testState = world.getBlockState(testPosition);
-                    if (!testState.isOf(Blocks.MAGMA_BLOCK) && testState.isSideSolidFullSquare(world, testPosition, Direction.UP) && world.isAir(testPosition.up())) {
+
+                    Optional<RegistryKey<Biome>> key = BuiltinRegistries.BIOME.getKey(world.getBiome(testPosition));
+
+                    if (key.isPresent() && !testState.isOf(Blocks.MAGMA_BLOCK) && testState.isSideSolidFullSquare(world, testPosition, Direction.UP) && world.isAir(testPosition.up()) && key.get() == CinderscapesBiomes.ASHY_SHOALS) {
                         world.setBlockState(testPosition.up(), CinderscapesBlocks.ASH.getDefaultState(), 2);
                     }
                 }
