bb6fc956f26383b9fb81a57163e0ce596a94bf88
Vaerian <vaeriann@gmail.com>
2020-10-31 02:41:37 -0600

    Fixed Zoglins spawning in mid-air
    Closes #48

--- src/main/java/com/terraformersmc/cinderscapes/Cinderscapes.java
+++ src/main/java/com/terraformersmc/cinderscapes/Cinderscapes.java
@@ -4,7 +4,12 @@ import com.terraformersmc.cinderscapes.config.CinderscapesConfig;
 import com.terraformersmc.cinderscapes.init.*;
 import com.terraformersmc.cinderscapes.util.NoiseCollisionChecker;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.mixin.object.builder.SpawnRestrictionAccessor;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.mob.ZoglinEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.Heightmap;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,10 +19,10 @@ public class Cinderscapes implements ModInitializer {
 	public static final String NAMESPACE = "cinderscapes";
 	public static final Logger LOGGER = LogManager.getLogger(StringUtils.capitalize(NAMESPACE));
 
-	// TODO: Add back a better implementation of the fog density in the Ashy Shoals
-
 	@Override
 	public void onInitialize() {
+		try { SpawnRestrictionAccessor.callRegister(EntityType.ZOGLIN, SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, ZoglinEntity::canMobSpawn); } catch (IllegalStateException e) { }
+
 		CinderscapesConfig.init();
 
 		CinderscapesItems.init();
