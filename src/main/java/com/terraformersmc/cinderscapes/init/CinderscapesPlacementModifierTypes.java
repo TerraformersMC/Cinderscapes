47826c2fd0f39d4ce7f74910ee4d710d627131fd
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-03-22 23:57:25 -0400

    made PlacementModifierTypes final

--- src/main/java/com/terraformersmc/cinderscapes/init/CinderscapesPlacementModifierTypes.java
+++ src/main/java/com/terraformersmc/cinderscapes/init/CinderscapesPlacementModifierTypes.java
@@ -10,11 +10,9 @@ import net.minecraft.world.gen.placementmodifier.PlacementModifierType;
 
 // TODO: Check
 public class CinderscapesPlacementModifierTypes {
-    public static PlacementModifierType<SafelistRangeCeilingPlacementModifier> COUNT_CEILING;
-    public static PlacementModifierType<SafelistRangeFloorPlacementModifier> COUNT_FLOOR;
+    public static final PlacementModifierType<SafelistRangeCeilingPlacementModifier> COUNT_CEILING = Registry.register(Registry.PLACEMENT_MODIFIER_TYPE, new Identifier("cinderscapes", "safelist_range_ceiling"), () -> SafelistRangeCeilingPlacementModifier.UNIFORM_CODEC);
+    public static final PlacementModifierType<SafelistRangeFloorPlacementModifier> COUNT_FLOOR  = Registry.register(Registry.PLACEMENT_MODIFIER_TYPE, new Identifier("cinderscapes", "safelist_range_floor"), () -> SafelistRangeFloorPlacementModifier.UNIFORM_CODEC);
 
     public static void init() {
-        COUNT_CEILING = Registry.register(Registry.PLACEMENT_MODIFIER_TYPE, new Identifier("cinderscapes", "safelist_range_ceiling"), () -> SafelistRangeCeilingPlacementModifier.UNIFORM_CODEC);
-        COUNT_FLOOR = Registry.register(Registry.PLACEMENT_MODIFIER_TYPE, new Identifier("cinderscapes", "safelist_range_floor"), () -> SafelistRangeFloorPlacementModifier.UNIFORM_CODEC);
     }
 }
