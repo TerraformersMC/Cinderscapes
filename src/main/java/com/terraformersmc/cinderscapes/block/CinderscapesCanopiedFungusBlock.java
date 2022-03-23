200e31ae3a07580bd04f682c63e1204e1599c08a
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2022-03-22 23:19:49 -0400

    fixed runtime crash

--- src/main/java/com/terraformersmc/cinderscapes/block/CinderscapesCanopiedFungusBlock.java
+++ src/main/java/com/terraformersmc/cinderscapes/block/CinderscapesCanopiedFungusBlock.java
@@ -6,6 +6,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.FungusBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 
@@ -13,20 +14,20 @@ import java.util.Random;
 import java.util.function.Supplier;
 
 public class CinderscapesCanopiedFungusBlock extends FungusBlock {
-    private final Supplier<ConfiguredFeature<CanopiedHugeFungusFeatureConfig, ?>> config;
+    private final Supplier<RegistryEntry<ConfiguredFeature<CanopiedHugeFungusFeatureConfig, ?>>> config;
 
-    public CinderscapesCanopiedFungusBlock(Settings settings, Supplier<ConfiguredFeature<CanopiedHugeFungusFeatureConfig, ?>> supplier) {
+    public CinderscapesCanopiedFungusBlock(Settings settings, Supplier<RegistryEntry<ConfiguredFeature<CanopiedHugeFungusFeatureConfig, ?>>> supplier) {
         super(settings, null);
         this.config = supplier;
     }
 
     public boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
-        Block block = ((this.config.get()).config()).soilBlock.getBlock();
+        Block block = ((this.config.get().value()).config()).soilBlock.getBlock();
         Block block2 = world.getBlockState(pos.down()).getBlock();
         return block2 == block;
     }
 
     public void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
-        (this.config.get()).generate(world, world.getChunkManager().getChunkGenerator(), random, pos);
+        (this.config.get().value()).generate(world, world.getChunkManager().getChunkGenerator(), random, pos);
     }
 }
