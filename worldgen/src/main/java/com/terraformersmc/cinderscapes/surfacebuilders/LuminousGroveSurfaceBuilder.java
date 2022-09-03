aeab03039f0434fb58f2bbeae9961c8f784ab363
gniftygnome <minecraft@gnifty.net>
2022-09-03 02:15:16 -0700

    Switch Luminous Grove surface generation to a surface builder.
    - Fix Luminous Grove surface generation when TerraBlender is present

---
+++ worldgen/src/main/java/com/terraformersmc/cinderscapes/surfacebuilders/LuminousGroveSurfaceBuilder.java
@@ -0,0 +1,47 @@
+package com.terraformersmc.cinderscapes.surfacebuilders;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.tag.BiomeTags;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.source.BiomeAccess;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.gen.chunk.BlockColumn;
+
+public class LuminousGroveSurfaceBuilder extends CinderscapesSurfaceBuilder {
+	private final BlockState topMaterial;
+	private final BlockState midMaterial;
+	private final BlockState lowMaterial;
+
+    public LuminousGroveSurfaceBuilder(BlockState topMaterial, BlockState midMaterial, BlockState lowMaterial) {
+        this.topMaterial = topMaterial;
+        this.midMaterial = midMaterial;
+        this.lowMaterial = lowMaterial;
+    }
+
+    @Override
+    public void generate(BiomeAccess biomeAccess, BlockColumn column, Random rand, Chunk chunk, Biome biome, int x, int z, int vHeight, int seaLevel) {
+        BlockPos pos = new BlockPos(x, -128, z);
+        boolean inAir = false;
+
+        // Set in-biome netherrack
+        for (int y = chunk.getTopY() - 6; y >= seaLevel - 1; --y) {
+            BlockState state = column.getState(y);
+            if (state.isAir()) {
+                inAir = true;
+            } else {
+                if (inAir && state.isOf(lowMaterial.getBlock()) && biomeAccess.getBiome(pos.withY(y)).matchesKey(biomeKey)) {
+                    column.setState(y, rand.nextFloat() < 0.99f ? topMaterial : midMaterial);
+                }
+                inAir = false;
+            }
+        }
+    }
+
+    @Override
+    public boolean filterBiome(RegistryEntry<Biome> biome) {
+        return biome.isIn(BiomeTags.IS_NETHER);
+    }
+}
